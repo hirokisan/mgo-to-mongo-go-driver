d860da95f51a86a05950ed35f5db6c5566d8d6e4
hirokisan <dev@hirokiueno.com>
2022-01-19 23:05:11 +0900

    do not need model

--- pkg/mgo/session_test.go
+++ pkg/mgo/session_test.go
@@ -5,22 +5,25 @@ import (
 
 	"github.com/hirokisan/mgo/bson"
 
-	"github.com/hirokisan/mgo-to-mongo-go-driver/model"
 	"github.com/hirokisan/mgo-to-mongo-go-driver/pkg/mgo/mgotest"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+type Target struct {
+	ID int `bson:"id"`
+}
+
 func TestInsert(t *testing.T) {
 	session := mgotest.NewTestSession()
 	defer session.Close()
 
-	col := session.Collection("cup")
-	cup := model.Cup{ID: 1}
-	require.NoError(t, col.Insert(cup))
+	col := session.Collection("target")
+	target := Target{ID: 1}
+	require.NoError(t, col.Insert(target))
 
-	var got model.Cup
-	require.NoError(t, col.Find(bson.M{"id": cup.ID}).One(&got))
+	var got Target
+	require.NoError(t, col.Find(bson.M{"id": target.ID}).One(&got))
 
-	assert.Equal(t, got.ID, cup.ID)
+	assert.Equal(t, got.ID, target.ID)
 }
--- pkg/mongodriver/session_test.go
+++ pkg/mongodriver/session_test.go
@@ -4,24 +4,27 @@ import (
 	"context"
 	"testing"
 
-	"github.com/hirokisan/mgo-to-mongo-go-driver/model"
 	"github.com/hirokisan/mgo-to-mongo-go-driver/pkg/mongodriver/mongodrivertest"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type Target struct {
+	ID int `bson:"id"`
+}
+
 func TestInsert(t *testing.T) {
 	ctx := context.Background()
 	db := mongodrivertest.NewTestDatabase(ctx)
 
-	col := db.Collection("cup")
-	cup := model.Cup{ID: 1}
-	_, err := col.InsertOne(ctx, cup)
+	col := db.Collection("target")
+	target := Target{ID: 1}
+	_, err := col.InsertOne(ctx, target)
 	require.NoError(t, err)
 
-	var got model.Cup
-	require.NoError(t, col.FindOne(ctx, primitive.M{"id": cup.ID}).Decode(&got))
+	var got Target
+	require.NoError(t, col.FindOne(ctx, primitive.M{"id": target.ID}).Decode(&got))
 
-	assert.Equal(t, got.ID, cup.ID)
+	assert.Equal(t, got.ID, target.ID)
 }
